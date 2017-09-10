9809732d6dd5c8e286fb8dcf4e8f5f2ef031d28b
Goryansky Roman <rgoryansky@gmail.com>
2017-09-10 19:14:24 +0800

    Добавьте файлы проекта.

---
+++ TestJob v2/Dictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestJob_v2
+{
+    class Dictionary
+    {
+        public List<string> readedwords;            //считанные слова из файла
+        public List<string> selection;              //выборка из 5 слов
+
+        public Dictionary(string path)
+        {
+            string[] input = File.ReadAllLines(path, Encoding.GetEncoding(1251));
+            readedwords = new List<string>(input);
+            selection = new List<string>();
+        }
+        public IEnumerable<string> Filling(string demand)       //выбираем все подходящие под запрос слова
+        {
+            readedwords.Sort();
+            int i = readedwords.BinarySearch(demand);           //бинарный поиск
+            if (i < 0)
+                i = -i - 1;
+            while (i >= 0 && readedwords[i].StartsWith(demand))         //если вернули не самое первое соответсвие запросу
+                --i;                                                //поднимаемся наверх, к самому первому
+            ++i;
+            while (i < readedwords.Count && readedwords[i].StartsWith(demand))      //возвращаем все слова, соответствующие нашему запросу
+            {
+                yield return readedwords[i];
+                ++i;
+            }
+
+        }
+        public void Complete(string request)            //выбираем 5 лучших
+        {
+            var suitable = Filling(request);
+            foreach (var str in suitable)
+            {
+                if (selection.Count >= 5)
+                    break;
+                selection.Add(str);                   //т.к слова отсортированы, выбираем 5 первых из всех
+                Console.WriteLine(str);
+            }
+            if (selection.Count == 0)
+                Console.WriteLine("No matches found");
+        }
+    }
+}
---
+++ TestJob v2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+namespace TestJob_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please, enter path to the file - ");
+                string path = Console.ReadLine();
+                Dictionary words = new Dictionary(path);
+                Console.Write("Please, enter your word: ");
+                string request = Console.ReadLine();
+                if (request == "###")
+                {
+                    Console.WriteLine("Application complete");
+                    Thread.Sleep(2000);
+                    return;
+                }
+                else
+                {
+                    words.Complete(request);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
